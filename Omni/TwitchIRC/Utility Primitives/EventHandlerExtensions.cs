036cfc3ca607d193636d131e87074ba289257bc5
Nikhil <nikhiln@myitsupport.com>
2016-09-05 01:19:05 -0500

    Add proper exception handling for async invoke of event handlers.

--- Omni/TwitchIRC/Utility Primitives/EventHandlerExtensions.cs
+++ Omni/TwitchIRC/Utility Primitives/EventHandlerExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,13 +14,22 @@ namespace TwitchIRC.Utility_Primitives
             foreach (EventHandler<T> r in Handler.GetInvocationList())
             {
                 TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
-                r.BeginInvoke(sender, e, new AsyncCallback((IAsyncResult ar) =>
-                {
-                    r.EndInvoke(ar);
-                    tcs.TrySetResult(true);
-                }), null);
+                r.BeginInvoke(sender, e, new AsyncCallback(_CompleteAsyncInvoke<T>), tcs);
                 await tcs.Task;
             }
+        }
+        internal static void _CompleteAsyncInvoke<T>(IAsyncResult ar)
+        {
+            EventHandler<T> r = (ar as AsyncResult)?.AsyncDelegate as EventHandler<T>;
+            TaskCompletionSource<bool> tcs = (ar as AsyncResult)?.AsyncState as TaskCompletionSource<bool>;
+            try
+            {
+                r.EndInvoke(ar);
+            }catch(Exception ex)
+            {
+                tcs?.TrySetException(ex);
+            }
+            tcs?.TrySetResult(true);
         }
     }
 }
